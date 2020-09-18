464db3caa911c80f057c9a095efb17cc17a515b6
Nathanael Carauna <Nathanael.carauna@gmail.com>
2020-09-17 22:43:52 -0300

    Adjustments

--- PythonSolutions/URI/1110_jogando_cartas_fora.py
+++ PythonSolutions/URI/1110_jogando_cartas_fora.py
@@ -1,14 +1,13 @@
 while True:
     n = int(input())
-
     if n == 0:
         break
-
     pilha = []
+    
     for i in range (1, n+1):
         pilha.append(i)
-
     discartadas = []
+
     while len(pilha) >= 2:
         discartadas.append(pilha.pop(0))
         pilha.insert(len(pilha)-1, pilha.pop(0))
